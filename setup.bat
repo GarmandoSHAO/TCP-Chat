74e1868d47eaaf9aa132206d6224143ae6b6efb2
Garmando <14117102+garmando@user.noreply.gitee.com>
2026-06-19 01:40:50 +0800

    fix: 添加解压步骤，解压出错时显示错误信息

--- setup.bat
+++ setup.bat
@@ -5,9 +5,21 @@ cd /d "%~dp0"
 echo Downloading from Gitee...
 powershell -Command "$p=[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Invoke-WebRequest 'https://gitee.com/garmando/tcp-chat/repository/archive/main.zip' -OutFile 'TCP-Chat.zip' -ErrorAction Stop"
 
-if exist "TCP-Chat.zip" (
-    echo Saved: %cd%\TCP-Chat.zip
-) else (
+if not exist "TCP-Chat.zip" (
     echo Failed
+    pause
+    exit /b
+)
+
+echo Extracting...
+tar -xf "TCP-Chat.zip" 2>&1
+del "TCP-Chat.zip" 2>nul
+for /d %%D in (*) do if exist "%%D\main.py" move "%%D" "TCP-Chat" >nul 2>&1
+if not exist "TCP-Chat" for /d %%D in (*) do if exist "%%D\tcp_chat" move "%%D" "TCP-Chat" >nul 2>&1
+
+if exist "TCP-Chat\main.py" (
+    echo OK - %cd%\TCP-Chat
+) else (
+    echo Extraction failed
 )
 pause
