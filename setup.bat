188fcc0009879a58a07c8027ae19428d9440c757
Garmando <14117102+garmando@user.noreply.gitee.com>
2026-06-19 01:39:09 +0800

    refactor: setup.bat 仅下载，不进行解压等后续步骤

--- setup.bat
+++ setup.bat
@@ -1,29 +1,13 @@
 @echo off
 chcp 65001 >nul 2>&1
-title TCP Chat Setup
 cd /d "%~dp0"
 
 echo Downloading from Gitee...
-echo.
+powershell -Command "$p=[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Invoke-WebRequest 'https://gitee.com/garmando/tcp-chat/repository/archive/main.zip' -OutFile 'TCP-Chat.zip' -ErrorAction Stop"
 
-powershell -Command "$p=[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; try { Invoke-WebRequest 'https://gitee.com/garmando/tcp-chat/repository/archive/main.zip' -OutFile 'TCP-Chat.zip' -ErrorAction Stop; Write-Host 'OK' } catch { Write-Host 'FAIL'; exit 1 }"
-
-if not exist "TCP-Chat.zip" (
-    echo Download failed.
-    pause
-    exit /b
-)
-
-echo Extracting...
-if exist "TCP-Chat" rmdir /s /q "TCP-Chat" 2>nul
-tar -xf "TCP-Chat.zip" 2>nul
-del "TCP-Chat.zip" 2>nul
-for /d %%D in (*) do if exist "%%D\main.py" move "%%D" "TCP-Chat" >nul 2>&1
-if not exist "TCP-Chat" for /d %%D in (*) do if exist "%%D\tcp_chat" move "%%D" "TCP-Chat" >nul 2>&1
-
-if exist "TCP-Chat\TCP-Chat.exe" (
-    echo Done! Files in: %cd%\TCP-Chat
+if exist "TCP-Chat.zip" (
+    echo Saved: %cd%\TCP-Chat.zip
 ) else (
-    echo Extraction failed.
+    echo Failed
 )
 pause
