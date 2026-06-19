e2f68ecfd32c467a8887bb554d193d2b6e8d868e
Garmando <14117102+garmando@user.noreply.gitee.com>
2026-06-19 11:15:37 +0800

    fix: 隧道改为服务端启动后才建，配置页不预连

--- tcp_chat/ui/app.py
+++ tcp_chat/ui/app.py
@@ -109,14 +109,6 @@ class ChatClientUI:
         self.create_frame, self.create_entries = build_create_room_view(
             self.root, self._on_create_room, self._back_to_start)
         self._wan_entry = self.create_entries.get("外网IP")
-        # 一进入创建页面就开始连隧道
-        addr = self.create_entries["局域网IP:端口"].get()
-        port = get("default_port", 8888)
-        if ":" in addr:
-            p = addr.rsplit(":", 1)[1]
-            if p.isdigit():
-                port = int(p)
-        self._start_tunnel(port)
 
     def _back_to_start(self):
         if hasattr(self, 'create_frame'):
